dba73bb4408e6b32c29e6cc04a38bae01c17ae1f
Rudvika1402 <rudravikashini14@gmail.com>
2024-09-04 11:40:53 +0530

    Add files via upload

---
+++ htmlTemplate.py
@@ -0,0 +1,45 @@
+css = '''
+<style>
+.chat-message {
+    padding: 1.5rem; border-radius: 0.5rem; margin-bottom: 1rem; display: flex
+}
+.chat-message.user {
+    background-color: #2b313e
+}
+.chat-message.bot {
+    background-color: #475063
+}
+.chat-message .avatar {
+  width: 20%;
+}
+.chat-message .avatar img {
+  max-width: 78px;
+  max-height: 78px;
+  border-radius: 50%;
+  object-fit: cover;
+}
+.chat-message .message {
+  width: 80%;
+  padding: 0 1.5rem;
+  color: #fff;
+}
+'''
+
+
+bot_template = '''
+<div class="chat-message bot">
+    <div class="avatar">
+        <img src="https://static.vecteezy.com/system/resources/previews/022/976/585/original/chat-bot-icon-with-artificial-intelligence-illustration-of-a-cute-robot-in-flat-style-on-a-blue-background-vector.jpg" style="max-height: 78px; max-width: 78px; border-radius: 50%; object-fit: cover;">
+    </div>
+    <div class="message">{{MSG}}</div>
+</div>
+'''
+
+user_template = '''
+<div class="chat-message user">
+    <div class="avatar">
+        <img src="https://pbs.twimg.com/media/FjU2lkcWYAgNG6d.jpg">
+    </div>    
+    <div class="message">{{MSG}}</div>
+</div>
+'''
